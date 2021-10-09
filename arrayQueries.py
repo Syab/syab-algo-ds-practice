48c23600e9a1e0c8838a40b16fb6903e3dec816b
syab <syabster.phang18@gmail.com>
2021-10-09 10:53:48 +0800

    modified arrayQueries

--- arrayQueries.py
+++ arrayQueries.py
@@ -12,18 +12,42 @@ def solveOne(ar, Q, inputar):
     r1=Q[1]
     r2=Q[2]
     addVal=Q[3]
-    # INCOMPLETE ############
-    return
+    count=0
+    for elem in range(r1-1, r2):
+        ar[elem] = ar[elem] + addVal
+    # print (ar)
+    return (ar,count)
 
+# WRONG SOLUTION
 def solveTwo(ar, Q, inputar):
-    return
+    most_occurred_count = 0
+    count = 0
+    remainder = 0
+    remlist = []
+    r1=Q[1]
+    r2=Q[2]
+    divisor=inputar[1]
+    for elem in range(r1-1, r2):
+        remainder=ar[elem]%divisor
+        # print (remainder)
+        remlist.append(remainder)
+    
+    # print(remlist)
+    occurence_count = collections.Counter(remlist)
+    most_occurred_count = occurence_count.most_common(1)[0][1]
+    print(occurence_count)
+    count = int(inputar[0]) - int(most_occurred_count)
+    return (ar,count)
 
 def solve(startar,quer,inputar):
     val = 0
     if (quer[0]==1):
         solveOne(startar, quer, inputar)
+        startar = startar
     elif (quer[0]==2):
-        solveTwo(startar, quer, inputar)
+        result=solveTwo(startar, quer, inputar)
+        startar= result[0]
+        val=result[1]
     return (startar, val)
 
 inputar = input().split()
@@ -31,9 +55,8 @@ inputar = list(map(int, inputar))
 if (len(inputar) != 3):
     print("Expected number of space separated inputs is 3.")
     exit()
-N = inputar[0]
-K = inputar[1]
-Q = inputar[2]
+    
+N, K, Q = inputar[0],inputar[1],inputar[2] 
 
 ar = input().split()
 ar = list(map(int, ar)) 
@@ -47,7 +70,7 @@ for i in range(Q):
     queryar = list(map(int,queryar))
     if (len(queryar)==3 or len(queryar)==4):
         listOfQuer.append(queryar)
-        print (listOfQuer)
+        # print (listOfQuer)
     else:
         print ("Queries do not meet minimum length")
         exit()
@@ -56,7 +79,5 @@ for j in listOfQuer:
     output_= 0
     startar= ar
     output_ = solve(startar,j,inputar)
-    print(output_)
-
-# output_ = solve(ar, N)
-# print (output_)
+    if ( j[0] == 2):
+        print(output_[1])
